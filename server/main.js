44c4fdda49e86e59c60eec4db930afaa4c6dd004
bklynate <nathanielcfa@gmail.com>
2017-06-24 23:27:21 -0400

    [snapshot] keeping a reference to middleware structure

--- server/main.js
+++ server/main.js
@@ -2,17 +2,37 @@ import { Meteor } from "meteor/meteor";
 import { WebApp } from "meteor/webapp"
 import "./../imports/startup/simple-schema-configuration";
 import "./../imports/api/users";
-import "./../imports/api/links";
+import { Links } from "./../imports/api/links";
 
 Meteor.startup(() => {
   // code to run on server at startup
   WebApp.connectHandlers.use((request, response, next) => {
+    const _id = request.url.slice(1);
+    const link = Links.findOne({ _id });
+
+    if(link) {
+      response.statusCode = 302;
+      response.setHeader("Location", link.url)
+      response.end();
+    } else {
+      next()
+    }
+  });
+
+  WebApp.connectHandlers.use((request, response, next) => {
     console.log("Hey from middleware")
-    // res.statusCode = 404;
-    // res.setHeader("deez-nutz-og", "my stuff was here!");
-    // res.write("<h1>stfu when grown folks talking</h1>");
-    // res.end()
+    // Sets a status code
+    // response.statusCode = 404;
+
+    // Sets a header
+    // response.setHeader("deez-nutz-og", "my stuff was here!");
+
+    // Allow you to write to the html body
+    // response.write("<h1>stfu when grown folks talking</h1>");
+
+    // Ends the cycle
+    // response.end()
     next();
-  })
+  });
   console.log("the situation was dire... decisions had to be made..");
 });
