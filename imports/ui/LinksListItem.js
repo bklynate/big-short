218a4206853db5e6281cd1cf5dd65e72e573e778
bklynate <nathanielcfa@gmail.com>
2017-07-04 02:55:42 -0400

    [add] added user feedback to alert when link is copied

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,24 +2,42 @@ import React from "react";
 import Clipboard from "clipboard";
 
 export default class LinksListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    }
+  }
+
   componentDidMount() {
     this.clipboard = new Clipboard(this.refs.copy);
 
     this.clipboard.on("success", () => {
-      alert("Successfully copied url!")
+      this.setState({
+        copied: true
+      })
+      console.log(this.state.copied)
+      setTimeout(() => {
+        this.setState({
+          copied: false
+        })
+        console.log(this.state.copied)
+      }, 400);
     }).on("error", () => {
       alert("Your browser doesn't support this feature - Welcome to 2017 it's time to upgrade")
     })
   }
+
   componentWillUnMount() {
     this.clipboard.destory();
   }
+
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
-        <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>{this.state.copied ? "Copied" : "Copy" }</button>
       </div>
     )
   }
