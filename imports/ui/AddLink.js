477231a49b80ad32194cd2c500f1707b50c77c82
bklynate <nathanielcfa@gmail.com>
2017-08-27 08:26:35 -0400

    [edit] made inputs controlled

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,33 +2,40 @@ import React from "react";
 import { Meteor } from "meteor/meteor";
 
 export default class AddLink extends React.Component {
-  // constructor(props){
-  //   super(props)
-  //   this.state = {
-  //     url: "Nate lives!"
-  //   }
-  // }
-  onSubmit(e) {
-    e.preventDefault();
-
-    const url = this.refs.url.value.trim();
-
-    if (url) {
-      Meteor.call("links.insert", url);
-      this.refs.url.value = "";
+  constructor(props){
+    super(props)
+    this.state = {
+      url: ""
     }
   }
+  onSubmit(e) {
+    e.preventDefault();
+    // const url = this.state.url; These are the same this
+    const { url } = this.state; // object destructing way
 
+    if (url) {
+      Meteor.call("links.insert", url, (error, response) => {
+        if(!err) {
+          this.setState({ url: "" });
+        }
+      });
+    }
+  }
+  onChange(e) {
+    this.setState({
+      url: e.target.value.trim()
+    });
+  }
   render() {
     return (
       <div>
         <p>Add Link</p>
         <form onSubmit={this.onSubmit.bind(this)}>
           <input
-            // value={this.state.url}
+            value={this.state.url}
             type="text"
-            ref="url"
-            placeholder="Add a url here"/>
+            placeholder="Add a url here"
+            onChange={this.onChange.bind(this)}/>
           <button>Add Link</button>
         </form>
       </div>
--- imports/ui/ListFilter.js
+++ imports/ui/ListFilter.js
@@ -1,12 +1,31 @@
 import React from "react";
 import {Session} from "meteor/session";
+import { Tracker } from "meteor/tracker";
 
 export default class ListFilter extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      showVisible: false
+    }
+  }
+  
+  componentDidMount() {
+    this.filterTracker = Tracker.autorun(()=>{
+      const showVisible = Session.get("showVisible")
+      this.setState({showVisible: showVisible})
+    });
+  }
+
+  componentWillUnMount() {
+    this.filterTracker.stop()
+  }
+
   render() {
     return (
       <div>
         <label>
-          <input type="checkbox" onChange={(e)=>{Session.set("showVisible", !e.target.checked)}}/>
+          <input type="checkbox" checked={!this.state.showVisible} onChange={(e)=>{Session.set("showVisible", !e.target.checked)}}/>
           show hidden links
         </label>
       </div>
